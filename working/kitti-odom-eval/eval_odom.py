6ab78492642b82a6396cb50793f950ef9f0d2760
Thippeswamy <thippeswamy636408@gmail.com>
2025-06-22 15:43:39 +0530

    adding results files

--- working/kitti-odom-eval/eval_odom.py
+++ working/kitti-odom-eval/eval_odom.py
@@ -8,8 +8,8 @@ parser = argparse.ArgumentParser(description='KITTI evaluation')
 parser.add_argument('--result', type=str, required=True,
                     help="Result directory")
 parser.add_argument('--align', type=str,
-                    choices=['scale', 'scale_7dof', '7dof', '6dof'],
-                    default=None,
+                    choices=['scale', 'scale_7dof', '7dof', '6dof','None'],
+                    default='None',
                     help="alignment type")
 parser.add_argument('--seqs',
                     nargs="+",
@@ -24,6 +24,7 @@ result_dir = args.result
 
 # continue_flag = input("Evaluate result in {}? [y/n]".format(result_dir))
 # if continue_flag == "y":
+print('args.align',args.align)
 eval_tool.eval(
     gt_dir,
     result_dir,
